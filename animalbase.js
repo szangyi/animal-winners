36a14b4a5abd94e4d1f0220d9686c5fdcd9acd03
szangyi <alexandra.mszrs@gmail.com>
2021-02-22 18:11:07 +0100

    adding class

--- animalbase.js
+++ animalbase.js
@@ -120,6 +120,13 @@ function selectSort(event) {
   const sortBy = event.target.dataset.sort;
   const sortDir = event.target.dataset.sortDirection;
 
+  //find "old" sortby element and remove .sortBy
+  const oldElement = document.querySelector(`[data-sort='${settings.sortBy}']`);
+  oldElement.classList.remove("sortby");
+
+  //indicate active sort
+  event.target.classList.add("sortby");
+
   //toggle direction
   if (sortDir === "asc") {
     event.target.dataset.sortDirection = "desc";
